cf45053145c4d5c739d47ac24959a55d46834f86
masasifuriku <masasifuriku2000@gmail.com>
2024-01-16 14:21:25 +0900

    TitkeSceneのシーン遷移とESC終了を追加しました

---
+++ Assets/1/TitleContoroller.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleContoroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("PlayScene");
+        }
+        EndGame();
+    }
+    public void EndGame()
+    {
+        //エスケープキーを押したらゲームを終了する
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+            Application.Quit();
+#endif
+        }
+
+    }
+}
