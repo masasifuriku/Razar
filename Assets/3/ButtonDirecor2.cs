7db1ebd11a2854b38caa2ce878deac6bc39cdc83
TSITCL\it232227 <linxunfeng1@gmail.com>
2024-01-23 15:21:29 +0900

    Button押したらPlaySceneとかTitleSceneにいくようにしたかったけど出来んかったわ

--- Assets/3/ButtonDirecor2.cs
+++ Assets/3/ButtonDirecor2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonDirector : MonoBehaviour
+public class ButtonDirecor2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,6 @@ public class ButtonDirector : MonoBehaviour
     }
     public void OnClick()
     {
-        SceneManager.LoadScene("TitleScene");
+        SceneManager.LoadScene("PlayScene");
     }
 }
